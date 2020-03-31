5296c78b7d66c75123f1b5c43e000654f6cdee76
Prakhar Rathi <prakharrathi25@gmail.com>
2020-03-31 21:25:35 +0530

    HTML files created
    Created the index.html, base.html file required to display web page information and upload image

--- app/app.py
+++ app/app.py
@@ -1,15 +1,18 @@
 ''' Importing necessary libraries''' 
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
+# Creating an instance of the flask app
 app = Flask(__name__)
+Bootstrap(app) # Pass app into the bootstrap Class to use Bootstrap functions
 
 ''' 
 ROUTES
 '''
-
-@app.route('/', methods=['GET'])
+# Route when we recieve a GET request 
+@app.route('/', methods=['GET']) 
 def index():
-	return("Hello World")
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
